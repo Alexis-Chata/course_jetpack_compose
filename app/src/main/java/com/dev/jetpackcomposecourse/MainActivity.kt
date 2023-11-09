fce5492b378dad71f44188cb13ce32edf682fb0c
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-11-09 12:41:56 -0500

    Layout BOX

--- app/src/main/java/com/dev/jetpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/dev/jetpackcomposecourse/MainActivity.kt
@@ -3,13 +3,22 @@ package com.dev.jetpackcomposecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dev.jetpackcomposecourse.ui.theme.JetpackcomposecourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +28,8 @@ class MainActivity : ComponentActivity() {
             JetpackcomposecourseTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    MyBox()
                 }
             }
         }
@@ -39,5 +49,31 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     JetpackcomposecourseTheme {
         Greeting("Android")
+    }
+}
+
+// LAYOUTS JETPACK COMPOSE
+
+// BOX SERIA COMO UN FRAME DE LOS ANTIGUOS LAYOUTS DE XML
+@Composable
+fun MyBox() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .width(200.dp)
+                .height(200.dp)
+                .background(Color.Cyan)
+                .verticalScroll(rememberScrollState()), contentAlignment = Alignment.BottomCenter
+        ) {
+            Text(text = "Esto es un Ejemplo")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyBoxPreview() {
+    JetpackcomposecourseTheme {
+        MyBox()
     }
 }
