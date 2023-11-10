99b6da8721004214696d6bf20a26a8ffd1e2ac9c
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-11-09 23:48:20 -0500

    Layout ConstraintLayout

---
+++ app/src/main/java/com/dev/jetpackcomposecourse/ConstraintLayout.kt
@@ -0,0 +1,45 @@
+package com.dev.jetpackcomposecourse
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+
+// Constraint Layout
+// implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+@Preview
+@Composable
+fun MyConstraintLayout(){
+    ConstraintLayout(Modifier.fillMaxSize()) {
+        val (boxRed, boxBlue, boxGreen, boxYellow, boxMagenta) = createRefs()
+
+        Box(modifier = Modifier.size(125.dp).background(Color.Red).constrainAs(boxRed){
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            bottom.linkTo(parent.bottom)
+        })
+        Box(modifier = Modifier.size(125.dp).background(Color.Blue).constrainAs(boxBlue){
+            top.linkTo(boxRed.bottom)
+            end.linkTo(boxRed.start)
+        })
+        Box(modifier = Modifier.size(125.dp).background(Color.Green).constrainAs(boxGreen){
+            start.linkTo(boxRed.end)
+            top.linkTo(boxRed.bottom)
+        })
+        Box(modifier = Modifier.size(125.dp).background(Color.Yellow).constrainAs(boxYellow){
+            bottom.linkTo(boxRed.top)
+            end.linkTo(boxRed.start)
+        })
+        Box(modifier = Modifier.size(125.dp).background(Color.Magenta).constrainAs(boxMagenta){
+            bottom.linkTo(boxRed.top)
+            start.linkTo(boxRed.end)
+        })
+    }
+}
