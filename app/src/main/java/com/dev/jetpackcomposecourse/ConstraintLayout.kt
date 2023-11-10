8bc2a368d8ba861e87a71ee84d67ada67210fe63
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-11-09 23:53:26 -0500

    Layout ConstraintLayout Avanzado

--- app/src/main/java/com/dev/jetpackcomposecourse/ConstraintLayout.kt
+++ app/src/main/java/com/dev/jetpackcomposecourse/ConstraintLayout.kt
@@ -9,37 +9,154 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 
 // Constraint Layout
 // implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 @Preview
 @Composable
-fun MyConstraintLayout(){
+fun MyConstraintLayout() {
     ConstraintLayout(Modifier.fillMaxSize()) {
         val (boxRed, boxBlue, boxGreen, boxYellow, boxMagenta) = createRefs()
 
-        Box(modifier = Modifier.size(125.dp).background(Color.Red).constrainAs(boxRed){
-            top.linkTo(parent.top)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-            bottom.linkTo(parent.bottom)
-        })
-        Box(modifier = Modifier.size(125.dp).background(Color.Blue).constrainAs(boxBlue){
-            top.linkTo(boxRed.bottom)
-            end.linkTo(boxRed.start)
-        })
-        Box(modifier = Modifier.size(125.dp).background(Color.Green).constrainAs(boxGreen){
-            start.linkTo(boxRed.end)
-            top.linkTo(boxRed.bottom)
-        })
-        Box(modifier = Modifier.size(125.dp).background(Color.Yellow).constrainAs(boxYellow){
-            bottom.linkTo(boxRed.top)
-            end.linkTo(boxRed.start)
-        })
-        Box(modifier = Modifier.size(125.dp).background(Color.Magenta).constrainAs(boxMagenta){
-            bottom.linkTo(boxRed.top)
-            start.linkTo(boxRed.end)
-        })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                bottom.linkTo(parent.bottom)
+            })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Blue)
+            .constrainAs(boxBlue) {
+                top.linkTo(boxRed.bottom)
+                end.linkTo(boxRed.start)
+            })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Green)
+            .constrainAs(boxGreen) {
+                start.linkTo(boxRed.end)
+                top.linkTo(boxRed.bottom)
+            })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Yellow)
+            .constrainAs(boxYellow) {
+                bottom.linkTo(boxRed.top)
+                end.linkTo(boxRed.start)
+            })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Magenta)
+            .constrainAs(boxMagenta) {
+                bottom.linkTo(boxRed.top)
+                start.linkTo(boxRed.end)
+            })
+    }
+}
+
+
+// CREANDO GUIAS O LINEAS PARA USARLAS COMO REFERENCIAS
+// LA GUIA O LINEA SE ENGANCHA A SU PADRE CREO :)
+@Preview
+@Composable
+fun ConstraintExampleGuide() {
+    ConstraintLayout(Modifier.fillMaxSize()) {
+//        val startGuide = createGuidelineFromTop(16.dp)
+
+        val boxRed = createRef()
+        val topGuide = createGuidelineFromTop(0.1f)
+        val startGuide = createGuidelineFromStart(0.25f)
+
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                top.linkTo(topGuide)
+                start.linkTo(startGuide)
+            })
+
+    }
+}
+
+// CREANDO UNA BARRERA O LINEA PARA USARLAS COMO REFERENCIAS
+// LA BARRERA O LINEA SE ENGANCHA A LOS COMPONENTS
+@Preview
+@Composable
+fun ConstraintBarrier() {
+    ConstraintLayout(Modifier.fillMaxSize()) {
+        val (boxRed, boxGreen, boxYellow) = createRefs()
+        val barrier = createEndBarrier(boxRed, boxGreen)
+
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Green)
+            .constrainAs(boxGreen) {
+                start.linkTo(parent.start, margin = 16.dp)
+            })
+
+        Box(modifier = Modifier
+            .size(235.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                top.linkTo(boxGreen.bottom)
+                start.linkTo(parent.start, margin = 32.dp)
+            })
+
+        Box(
+            Modifier
+                .size(50.dp)
+                .background(Color.Yellow)
+                .constrainAs(boxYellow) {
+                    start.linkTo(barrier)
+                })
+    }
+}
+
+// CREANDO UNA CADENA DE COMPONENTS
+// LA CADENA SpreadInside PUEDE SEPARAR LOS COMPONENTS LO MAS QUE PUEDE
+// LA CADENA Spread PUEDE SEPARAR LOS COMPONENTS EQUITATIVAMENTE
+// LA CADENA Packed PUEDE JUNTAR LOS COMPONENTS LO MAS QUE PUEDE
+@Preview
+@Composable
+fun ConstraintChainExample() {
+    ConstraintLayout(Modifier.fillMaxSize()) {
+
+
+        val (boxRed, boxGreen, boxYellow) = createRefs()
+
+        Box(modifier = Modifier
+            .size(75.dp)
+            .background(Color.Green)
+            .constrainAs(boxGreen) {
+                start.linkTo(parent.start)
+                end.linkTo(boxRed.start)
+            })
+
+        Box(modifier = Modifier
+            .size(75.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                start.linkTo(boxGreen.end)
+                end.linkTo(boxYellow.start)
+            })
+
+        Box(
+            Modifier
+                .size(75.dp)
+                .background(Color.Yellow)
+                .constrainAs(boxYellow) {
+                    start.linkTo(boxRed.end)
+                    end.linkTo(parent.end)
+                })
+
+        createHorizontalChain(boxRed, boxGreen, boxYellow, chainStyle = ChainStyle.SpreadInside)
+        //createVerticalChain(boxRed, boxGreen, boxYellow, chainStyle = ChainStyle.SpreadInside)
+
     }
 }
